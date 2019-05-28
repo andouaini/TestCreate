8676b4ac77ac4a3d8865161b44433396573b9c97
zhaoluowei <1033467391@qq.com>
2019-05-28 17:48:29 +0800

    代码提交

--- src/main/java/com/wantup/test/controller/TestContrler.java
+++ src/main/java/com/wantup/test/controller/TestContrler.java
@@ -1,9 +1,21 @@
 package com.wantup.test.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Zhaoluowei
  * @version 1.0
  * @date 2019/5/28 13:21
  */
+@RestController
+@RequestMapping(value = "/test")
 public class TestContrler {
+
+    public String testMethodOne(){
+
+        return null;
+    }
+
+
 }
