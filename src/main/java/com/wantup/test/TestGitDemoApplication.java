ca236ae245cebcf4a53eb1d877608ee8a7af22fc
zhao luowei <1033467391@qq.com>
2019-05-28 13:02:03 +0800

    first commit

---
+++ src/main/java/com/wantup/test/TestGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wantup.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitDemoApplication.class, args);
+    }
+
+}
