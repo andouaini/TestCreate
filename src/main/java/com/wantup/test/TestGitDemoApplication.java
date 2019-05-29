980be3d01d102405ca2056a82a68bafc3da1c12b
zhaoluowei <1033467391@qq.com>
2019-05-29 11:57:23 +0800

    代码提交

--- src/main/java/com/wantup/test/TestGitDemoApplication.java
+++ src/main/java/com/wantup/test/TestGitDemoApplication.java
@@ -8,6 +8,16 @@ public class TestGitDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestGitDemoApplication.class, args);
+
+//        String s1 = "123";
+//        String s2 = "123";
+//
+//        System.out.println("s1 == s2"+(s1==s2));
+
+
+//        char c = 65;
+//        System.out.println("c = "+c);
+
     }
 
 }
